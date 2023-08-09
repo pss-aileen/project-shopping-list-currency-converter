6580f683c412b1e45048a7dc45221605009885b0
pss-aileen <140990100+pss-aileen@users.noreply.github.com>
2023-08-09 16:43:00 +0800

    #14 create array, but i can not combine...difficult

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 'use strict'
 {
 
+
   /////////////////////////////////
   // call api
   /////////////////////////////////
@@ -10,6 +11,21 @@
       const response = await fetch(
         "https://api.exchangerate.host/symbols",
       )
+      if (!response.ok) {
+        throw new Error(`HTTPS error: ${response.status}`);
+      }
+      const data = response.json();
+      return data;
+    } catch (error) {
+      console.error(`Could not get products: ${error}`);
+    }
+  }
+
+  // call json
+
+  async function callJson() {
+    try {
+      const response = await fetch("json/country-currency.json");
       if (!response.ok) {
         throw new Error(`HTTPS error: ${response.status}`);
       }
@@ -30,22 +46,85 @@
       for (const [s, d] of Object.entries(data)) {
         console.log(`${s}: ${d}`);
       }
-      data = Object.values(data["symbols"]);
+      // console.log(data);
+      return data = Object.values(data["symbols"]);
       // symbolsとdescriptionの配列、オブジェクトがかえる
-      console.log(data);
+    })
+    .then((data) => {
+      const callJsonPromise = callJson();
+      callJsonPromise
+        .then((data2) => {
+          combineArray(data, data2);
+        });
     });
+  
+  
 
-  function countryCodes(data) {
+  
+ 
+
+  
+  
+  function combineArray(data, data2) {
+    console.log(data);
+    console.log(data2);
     const array = [];
-    const test = Object.values(data["symbols"]);
 
-    const countrySet = new Object();
-    countrySet.code = "tiwatiwa";
-    countrySet.description = "tiwatiwa2";
+    pickCountryName(data, data2);
+    
+    for (let i = 0; i < data.length; i++) {
+      const currencyCode = data[i].code;
+      const currencyCodeName = data[i].description;
+  
+      const line = new Object;
+      line.currencyCode = currencyCode;
+      line.currencyCodeName = currencyCodeName;
+      line.countryName = "";
+      line.countryIcon = "";
 
-    console.log(array);
+      // currencyCode or CurrencyCodeNameを他のオブジェクトと照らし合わせる
+      // 検索させる
+      // 同じものがあれば、そこの配列から国名をひっこぬいてくる
+
+      // console.log(line);
+    }
   }
 
+  function pickCountryName(data1, data2) {
+    for (let i = 0; i < data1.length; i++) {
+      const countryName = data1[i].code;
+      // console.log(countryName);
+      for (let j = 0; j < data2.length; j++) {
+        const countryNameMatch = data2[j].currency_code;
+        if (countryName === countryNameMatch) {
+          const countrydata = data2[j].country;
+          // console.log(countrydata + i + ":" + + j);
+          console.log(`${i} ${data1[i].code} ${data1[i].description} ${j} ${data2[j].currency_code} ${data2[j].country}`);
+          // console.log(j);
+        }
+      }
+    }
+
+
+  }
+
+  function pickCountryFlag() {
+
+  }
+
+
+
+  
+
+  // シンボル一覧を1つ生成する
+  // from をクリックして、選んだら、そこに値を入れる
+  // to をクリックして、選んだら、checkedされた値をそこに入れる
+  // 国名と対応した国旗を出力させる、なければそれを出力する機能もつける
+
+  // カーレンシーの略と、国をリンクさせる
+  // 国とアイコン表示用の省略語をリンクさせる
+  // countryArray = [{ countryName: "COUNTRY NAME", currencyCode: "CDE", countryIcon: "am"}]; みたいな
+  // これを別フォルダで生成して保存しておいて、ここから情報をとってきて、APIにつっこむ
 
 
 } // end
