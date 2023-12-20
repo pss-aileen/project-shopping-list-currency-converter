38c27c1621980804e935c78a2db4d4b7a7a1abb2
pss-aileen <140990100+pss-aileen@users.noreply.github.com>
2023-12-20 17:08:55 +0800

    💕 FEATURE: 合計金額計算機能などなど追加、currency symbolsのデータ取得
    全体的に混乱してきたので今からはリファクタリングしていく

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 {
-
+  
   /******************************************************
     グローバル変数
   ******************************************************/
@@ -8,7 +8,41 @@
   let LOCAL_CURRENCY = "MYR";
   let FOREIGN_CURRENCY = "JPY";
   let RATE = 0;
-  const shoppingProducts = [];
+  const SHOPPING_LISTS = [];
+
+  /******************************************************
+    テストで共通化
+  ******************************************************/
+
+  /******************************************************
+    initLoad
+    - CURRENCY一覧を取得する、selectに入れる、初期値を設定する
+    - localStorageの内容確認、データがあれば反映させる（まずはfunctionでまとめる）
+    - 
+  ******************************************************/
+  
+  /******************************************************
+    Currency Symbols
+  ******************************************************/
+  
+  async function fetchSymbol() {
+    try {
+      // const response = await fetch("../json/Common-Currency.json");
+      const response = await fetch("https://gist.githubusercontent.com/ksafranski/2973986/raw/5fda5e87189b066e11c1bf80bbfbecb556cf2cc1/Common-Currency.json");
+      if (!response.ok) {
+        throw new Error("HTTP ERROR STATUS", response.status);
+      }
+
+      const data = await response.json();
+      console.log("DATA", data);
+      // return data.rates[this.foreignCurrency];
+    } catch (error) {
+      console.error("EROOR FETCHING DATA", error.message);
+    }
+  }
+
+  fetchSymbol();
+  
 
   /******************************************************
     ローカルストレージ
@@ -26,11 +60,19 @@
       "storageDate": new Date()
     };
 
-    const shoppingList = {
-
-    }
     localStorage.setItem("currencySetting", JSON.stringify(currencySetting));
-    localStorage.setItem("shoppingList", JSON.stringify(shoppingProducts));
+    localStorage.setItem("shoppingList", JSON.stringify(SHOPPING_LISTS));
+  }
+
+  /******************************************************
+   LocalStorage 呼び出し
+   ******************************************************/
+  
+  document.getElementById("btn-call-local-storage").addEventListener("click", () => {
+    CallFromLocalStorage();
+  });
+  
+  function CallFromLocalStorage() {
     console.log(localStorage.getItem("currencySetting"), localStorage.getItem("shoppingList"));
   }
 
@@ -80,12 +122,8 @@
     async setValues() {
       const localPrice = document.getElementById("local-price");
       const foreignPrice = document.getElementById("foreign-price");
-      // const localCurrency = document.getElementById("local-currency");
-      // const foreignCurrency = document.getElementById("foreign-currency");
 
       this.getRate().then(rate => {
-        // localCurrency.textContent = this.getLocalCurrencyString();
-        // foreignCurrency.textContent = this.getForeignCurrencyString();
         foreignPrice.textContent = (rate * localPrice.value).toFixed(4);
       });
      }
@@ -182,6 +220,7 @@
     }
   }
 
+
   /******************************************************
     Shopping List追加
   ******************************************************/
@@ -205,9 +244,9 @@
     const tr_local = document.createElement("tr");
     const tr_foreign = document.createElement("tr");
     const td_localValue = document.createElement("td");
-    const td_foreignlValue = document.createElement("td");
+    const td_foreignValue = document.createElement("td");
     const td_localCurrency = document.createElement("td");
-    const td_foreignlCurrency = document.createElement("td");
+    const td_foreignCurrency = document.createElement("td");
     const div_delete = document.createElement("div");
     const i_deleteIcon = document.createElement("i");
 
@@ -217,15 +256,13 @@
     div_delete.classList.add("shopping-list__delete");
     i_deleteIcon.classList.add("shopping-list__delete-icon", "bi", "bi-x-lg");
 
-
-
-
     span_productNmae.textContent = item.getProductNameString();
     td_localValue.textContent = item.getProductLocalPriceNumber();
-    td_foreignlValue.textContent = item.getProductForeignPriceNumber();
-    // td_foreignlValue.textContent = "外国通貨";
+    td_localValue.classList.add("get-product-local-price");
+    td_foreignValue.textContent = item.getProductForeignPriceNumber();
+    td_foreignValue.classList.add("get-product-foreign-price");
     td_localCurrency.textContent = LOCAL_CURRENCY;
-    td_foreignlCurrency.textContent = FOREIGN_CURRENCY;
+    td_foreignCurrency.textContent = FOREIGN_CURRENCY;
 
     const shoppingList = document.getElementById("shopping-list-content");
 
@@ -239,18 +276,58 @@
     table.appendChild(tr_foreign);
     tr_local.appendChild(td_localValue);
     tr_local.appendChild(td_localCurrency);
-    tr_foreign.appendChild(td_foreignlValue);
-    tr_foreign.appendChild(td_foreignlCurrency);
+    tr_foreign.appendChild(td_foreignValue);
+    tr_foreign.appendChild(td_foreignCurrency);
     div_delete.appendChild(i_deleteIcon);
 
     const object = {
+      "id": SHOPPING_LISTS.length,
       "productName": item.getProductNameString(),
       "localPrice": item.getProductLocalPriceNumber()
     }
 
-    shoppingProducts.push(object);
-    console.log(shoppingProducts);
+    SHOPPING_LISTS.push(object);
+
+    console.log(SHOPPING_LISTS);
+
+    const localAmount = document.getElementById("shopping-list-total-local-value");
+    const foreignAmount = document.getElementById("shopping-list-total-foreign-value");
+
+    let amount = 0;
+    for (let i = 0; i < SHOPPING_LISTS.length; i++) {
+      const value = Number(SHOPPING_LISTS[i]["localPrice"]);
+      amount = amount + value;
+    }
+    localAmount.textContent = amount;
+    foreignAmount.textContent = (amount * RATE).toFixed(2);
   });
+
+
+  document.getElementById("btn-recalc").addEventListener("click", () => {
+    reCalculateForeignValue(RATE);
+  });
+
+  function reCalculateForeignValue(RATE) {
+    const array = document.querySelectorAll(".get-product-local-price");
+    const array2 = document.querySelectorAll(".get-product-foreign-price");
+
+    console.log(array, array2);
+
+    // for (const value of array) { 
+    //   console.log(value.textContent);
+    // }
+
+    for (let i = 0; i < array.length; i++) {
+      console.log(array[i].textContent);
+      const localPrice = Number(array[i].textContent);
+      const NewForeignPrice = (localPrice * RATE).toFixed(2);
+      // console.log(array2[i].textContent);
+      array2[i].textContent = NewForeignPrice;
+      // console.log(array2[i].textContent);
+    }
+    
+  }
+
 
 
 }
--- project/todo.md
+++ project/todo.md
@@ -1,5 +1,28 @@
 # TODO
 
+
+# 共通化できるはず？のもの
+- LOCAL_CURRENCY と RATE を計算する関数
+
+- 値をあらゆる箇所にセットする関数
+  - localCurrency, foreignCurrency, shopping listの配列
+- 配列を展開する関数
+
+
+
+common currency https://gist.github.com/ksafranski/2973986
+
+## 整理
+- APIから選択肢をひっぱってくる
+- localCurrency値の決定
+- foreignCurrecy値の決定
+  - localCurrencyのAPIより、rateを計算
+  - shoppingListのcurrency変更
+  - shoppingList追加済みの商品のlocalPrice * rate = ForeignPriceを計算する
+
+
+- shopping list にidをつける
+
 ## 1219
 - 合計金額の表示
   - 配列に商品と金額を入れていく
@@ -10,6 +33,18 @@
   - 「設定を保存」
   - 「設定を呼び出す」
   - 最終的には常に保存しているデータを呼び出してくる
+  - データを呼び出す場所は？
+    - localCurrency, foreignCurrency
+    - shoppingList一覧の値全て
+    - 上記2つは連動している必要がある、rateの関係上
+  - rateが変更された場合の挙動をどうするか？
+    - foreignCurrencyの変更
+      - それぞれの金額、合計金額の再計算or再表示
+    - localCurrencyの変更
+      - すでにShopppingListに保存されている、表示されているものがある場合
+        - CAUTIONを出す（※レートが変更されているので、再度金額をご確認ください的な）
+        - 値段は一応設定は変えないようにしておく
+        - 一括でショッピングリストを削除できるようにしておく（設定済み）
 
 
 ## 段階
