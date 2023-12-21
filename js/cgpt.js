b193eced26580e16f4da501a7e5f24a7c26d6ff0
pss-aileen <140990100+pss-aileen@users.noreply.github.com>
2023-12-21 09:38:58 +0800

    🫶 REFACTOR: 1度しか使わない定数の削除、コードの並び替え、changeValueのcgptのリファクタリング例js追加

--- js/cgpt.js
+++ js/cgpt.js
@@ -1,80 +1,49 @@
-// イベントリスナーのコールバック関数を定義
-CreateListBtn.addEventListener("click", () => {
-  // HTML要素の取得
-  const productNameElement = document.getElementById("new-product");
-  const localPriceElement = document.getElementById("new-local-price");
+// リファクタリングコメント: 関数の宣言をアロー関数に変更し、コンパクトにまとめる
 
-  // 入力値の取得
-  const productName = productNameElement.value;
-  const localPrice = Number(localPriceElement.value);
+// リファクタリングコメント: 同じ DOM 要素の取得をまとめて、変数名を変更
+const getInputValue = (elementId) => document.getElementById(elementId).value;
 
-  // 新しいアイテムを作成
-  const div_item = createNewElement("div", "shopping-list__item");
-  const div_productName = createNewElement("div", "shopping-list__product-name");
-  const span_productNmae = createNewElement("span");
-  const div_productPrice = createNewElement("div", "shopping-list__product-price");
-  const table = createNewElement("table");
-  const [tr_local, tr_foreign] = createMultipleElements(["tr", "tr"]);
-  const [td_localValue, td_foreignValue, td_localCurrency, td_foreignCurrency] =
-      createMultipleElements(["td", "td", "td", "td"]);
-  const div_delete = createNewElement("div", "shopping-list__delete");
-  const i_deleteIcon = createNewElement("i", "shopping-list__delete-icon", "bi", "bi-x-lg");
+// リファクタリングコメント: 非同期関数の宣言をアロー関数に変更し、コンパクトにまとめる
+const fetchData = async (url) => {
+  const response = await fetch(url);
+  return response.json();
+};
 
-  // 要素にクラスを追加
-  span_productNmae.textContent = productName;
-  td_localValue.textContent = localPrice;
-  td_localValue.classList.add("get-product-local-price");
-  td_foreignValue.textContent = GET_foreignPrice(localPrice, 2);
-  td_foreignValue.classList.add("get-product-foreign-price");
-  td_localCurrency.textContent = LOCAL_CURRENCY;
-  td_foreignCurrency.textContent = FOREIGN_CURRENCY;
+// リファクタリングコメント: 非同期関数の宣言をアロー関数に変更し、コンパクトにまとめる
+const updateRate = async () => {
+  const data = await fetchData(`https://open.er-api.com/v6/latest/${LOCAL_CURRENCY}`);
+  RATE = data.rates[FOREIGN_CURRENCY];
+  console.log("UPDATED RATE", RATE);
+  return data.rates[FOREIGN_CURRENCY];
+};
 
-  // ショッピングリストに新しいアイテムを追加
-  const shoppingList = document.getElementById("shopping-list-content");
-  appendChildren(div_item, [div_productName, div_productPrice, div_delete]);
-  appendChildren(div_productName, [span_productNmae]);
-  appendChildren(div_productPrice, [table]);
-  appendChildren(table, [tr_local, tr_foreign]);
-  appendChildren(tr_local, [td_localValue, td_localCurrency]);
-  appendChildren(tr_foreign, [td_foreignValue, td_foreignCurrency]);
-  appendChildren(div_delete, [i_deleteIcon]);
-  shoppingList.appendChild(div_item);
+// リファクタリングコメント: 関数の呼び出しとログの出力をまとめる
+const setPrices = async () => {
+  console.log("SET PRICES");
+  const localPrice = document.getElementById("local-price");
+  const foreignPrice = document.getElementById("foreign-price");
 
-  // ショッピングリストデータを更新
-  const object = {
-      "id": SHOPPING_LISTS.length,
-      "productName": productName,
-      "localPrice": localPrice
-  };
-  SHOPPING_LISTS.push(object);
+  await updateRate();
+  foreignPrice.textContent = GET_foreignPrice(localPrice.value, 4);
+};
 
-  // 合計金額を更新
-  updateTotalAmount();
-});
+// リファクタリングコメント: 同じ DOM 要素の取得をまとめ、変数名を変更
+const updateShoppingListCurrency = () => {
+  const shoppingListLocalCurrency = document.getElementById("shopping-list-local-currency");
+  const shoppingListForeignCurrency = document.getElementById("shopping-list-foreign-currency");
+  shoppingListLocalCurrency.textContent = LOCAL_CURRENCY;
+  shoppingListForeignCurrency.textContent = FOREIGN_CURRENCY;
+};
 
-// 複数の子要素を持つ新しいHTML要素を作成
-function createNewElement(tagName, ...classNames) {
-  const element = document.createElement(tagName);
-  element.classList.add(...classNames);
-  return element;
+// リファクタリングコメント: 関数の呼び出しをまとめる
+function changeValue() {
+  updateInputCurrency();
+  setPrices();
+  updateShoppingListCurrency();
 }
 
-// 複数のHTML要素を作成して配列で返す
-function createMultipleElements(tags) {
-  return tags.map(tag => createNewElement(tag));
-}
-
-// 親要素に複数の子要素を追加
-function appendChildren(parent, children) {
-  children.forEach(child => parent.appendChild(child));
-}
-
-// 合計金額を更新する関数
-function updateTotalAmount() {
-  const localAmount = document.getElementById("shopping-list-total-local-value");
-  const foreignAmount = document.getElementById("shopping-list-total-foreign-value");
-
-  let amount = SHOPPING_LISTS.reduce((sum, item) => sum + item.localPrice, 0);
-  localAmount.textContent = amount;
-  foreignAmount.textContent = GET_foreignPrice(amount, 2);
-}
+// リファクタリングコメント: 同じ DOM 要素の取得をまとめ、変数名を変更
+const updateInputCurrency = () => {
+  LOCAL_CURRENCY = getInputValue("input-local-currency");
+  FOREIGN_CURRENCY = getInputValue("input-foreign-currency");
+};
